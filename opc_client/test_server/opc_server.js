20efe4f379bf985e50d1ad90515f7a2c27469b96
Pulkit <pulkitmlk1802@gmail.com>
2023-05-26 22:37:11 +0530

    Sample opc client and server

---
+++ opc_client/test_server/opc_server.js
@@ -0,0 +1,53 @@
+const opcua = require("node-opcua");
+
+const server = new opcua.OPCUAServer({
+    port: 4840, // Specify the port on which the server will listen
+    resourcePath: "/", // Specify the resource path for the server
+});
+
+server.initialize(() => {
+    console.log("OPC UA server initialized");
+
+    // Create a new address space
+    const addressSpace = server.engine.addressSpace;
+    const namespace = addressSpace.getOwnNamespace();
+
+    // Create a object folder
+    const device = namespace.addObject({
+        organizedBy: addressSpace.rootFolder.objects,
+        browseName: "MyDevice"
+    });
+
+    // Create a variable node in the address space
+    namespace.addVariable({
+        nodeId: "ns=1;s=Scalar_Simulation_String",
+        browseName: "Scalar_Simulation_String",
+        componentOf: device,
+        dataType: "String",
+        value: {
+            get: function () {
+                return new opcua.Variant({ dataType: opcua.DataType.String, value: "Hello OPC UA client!" });
+            },
+            set: function (variant) {
+                variable2 = parseFloat(variant.value);
+                return opcua.StatusCodes.Good;
+            }
+        },
+    });
+
+    // Start the server
+    
+});
+
+server.start(() => {
+    console.log("Server is now listening ... ( press CTRL+C to stop)");
+    const endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+    console.log(" the primary server endpoint url is ", endpointUrl );
+});
+
+process.on("SIGINT", () => {
+    server.shutdown(() => {
+        console.log("OPC UA server shutting down");
+        process.exit(0);
+    });
+});
