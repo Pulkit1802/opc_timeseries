be81447f27498c1ae54e073e22044b18c1017522
Pulkit <pulkitmlk1802@gmail.com>
2023-05-26 17:50:37 +0530

    Python profibus connection script

---
+++ profibus/connect.py
@@ -0,0 +1,29 @@
+from pyprofibus import Profibus
+
+
+def listen_to_profibus():
+    profibus = Profibus()
+    profibus.open()
+    profibus.set_baudrate(9600)
+    profibus.set_slave_address(1)
+
+    try:
+        while True:
+            data_to_send = [0x01, 0x02, 0x03] 
+
+            received_data = profibus.send_receive(data_to_send)
+            process_received_data(received_data)
+
+    except KeyboardInterrupt:
+        print("Closing connection to Profibus.")
+        profibus.close()
+
+    except Exception as e:
+        print("An error occured:", e)
+        profibus.close()
+
+def process_received_data(data):
+    print("Received data:", data)
+
+
+listen_to_profibus()
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,6 @@ const { fetch2DaysData } = require("./requestHandler");
 
 const router = Express.Router();
 
-// console.log(fetch2DaysData)
-
 router.route("/getData").get(fetch2DaysData);
 
 module.exports = {
